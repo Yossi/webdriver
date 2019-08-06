a3669cb34550ecbcb3f1db169dbee5e5443106ed
Yossi Rafelson <yossi.rafelson@gmail.com>
2019-08-06 14:56:45 -0700

    first commit

---
+++ driver.py
@@ -0,0 +1,80 @@
+import os
+import logging
+import platform
+from zipfile import ZipFile
+from io import BytesIO
+from time import sleep
+import requests
+from selenium import webdriver
+
+def make_executable(path):
+    mode = os.stat(path).st_mode
+    mode |= (mode & 0o444) >> 2    # copy R bits to X
+    os.chmod(path, mode)
+
+def update_chromedriver(version=''):
+    if not version:
+        version = requests.get('https://chromedriver.storage.googleapis.com/LATEST_RELEASE').text.strip()
+    arch = {'Linux': 'linux64', 'Darwin': 'mac64', 'Windows': 'win32'}[platform.system()] # these are the only options available
+    logging.info('%s detected', arch)
+    url = 'https://chromedriver.storage.googleapis.com/{}/chromedriver_{}.zip'.format(version, arch)
+    logging.info('downloading...')
+    zip_file = ZipFile(BytesIO(requests.get(url).content))
+    for name in zip_file.namelist():
+        if name.startswith('chromedriver'):
+            logging.info('unpacking...')
+            with open(name, 'wb') as out:
+                out.write(zip_file.read(name))
+            logging.info('ready')
+            break
+
+def get_driver(headless=False):
+    options = webdriver.ChromeOptions()
+    options.add_argument('log-level=3')
+    options.add_argument("user-data-dir=profile/")
+    if headless:
+        options.add_argument('headless')
+        options.add_argument('disable-gpu')
+
+    # assume chrome is installed. anything else is out of scope
+    # http://chromedriver.chromium.org/downloads/version-selection
+    if not (os.path.isfile('chromedriver') or os.path.isfile('chromedriver.exe')): # bootstrap self
+        logging.info('chromedriver not found')
+        update_chromedriver() # just get newest, dont worry about automatically matching chrome yet
+        make_executable('./chromedriver')
+
+    driver = webdriver.Chrome('./chromedriver', chrome_options=options) # assumes our version of chromedriver works with our version of chrome
+    chrome_version = driver.capabilities[({'version', 'browserVersion'} & driver.capabilities.keys()).pop()] #1
+    CD_version = driver.capabilities['chrome']['chromedriverVersion'].split()[0]
+    version_check_url = 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE_{}'.format(chrome_version.rpartition('.')[0]) #2
+    matching_CD_version = requests.get(version_check_url).text.strip() #3
+    if CD_version != matching_CD_version: #5
+        driver.quit() # need to release the file lock
+        logging.info('have chromedriver {}. attempting to update to {}'.format(CD_version, matching_CD_version))
+        update_chromedriver(matching_CD_version) #4
+        driver = webdriver.Chrome('./chromedriver', chrome_options=options) # reopen with fresh new chromedriver
+
+    return driver
+
+
+
+def test():
+    try:
+        driver = None
+        logging.info('get driver...')
+        driver = get_driver()
+        driver.implicitly_wait(5)
+        driver.set_window_size(1024, 768)
+        driver.get('https://www.google.com')
+        sleep(2)
+
+    finally:
+        if driver:
+            logging.info('cleanup')
+            driver.quit()
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s %(message)s',
+                        datefmt='%H:%M:%S')
+    test()
