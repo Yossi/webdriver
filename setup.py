c83d83a0d4aa9bcd699b35f9316c5a46883c3c1f
Yossi Rafelson <yossi.rafelson@gmail.com>
2019-08-07 14:19:14 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt', 'r') as f:
+    install_requires = f.read().strip().split('\n')
+
+setuptools.setup(
+    name="webdriver",
+    version="0.0.1",
+    author="Yossi",
+    description="webdriver factory",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Yossi/webdriver",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    license='Unlicense',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+    ],
+)
