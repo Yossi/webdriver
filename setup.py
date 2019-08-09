51cf2359d855a8b94bb84a59e5b5aadb8af6ed53
Yossi Rafelson <yossi.rafelson@gmail.com>
2019-08-09 14:27:03 -0700

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt', 'r') as f:
 
 setuptools.setup(
     name="webdriver",
-    version="0.0.1",
+    version="0.0.3",
     author="Yossi",
     description="webdriver factory",
     long_description=long_description,
